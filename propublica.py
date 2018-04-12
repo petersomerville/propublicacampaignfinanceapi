a70c1a1fc03e039a333cc6bc384bb40e612f6428
Peter <pesomerville@peter-somervilles-macbook-pro.local>
2018-04-12 19:19:17 +0800

    candidates loaded to db

--- propublica.py
+++ propublica.py
@@ -1,8 +1,11 @@
 import json, requests
 from pprint import pprint
+from ppds import Candidate
+
+from base import DbManager
 
 API_BASE = "https://api.propublica.org"
-API_KEY = "??????????????????????????????????????????"
+API_KEY = "esfTxmzDJFzCaY4UsKzjyqaDlFPYE4lD5lxqmvz6"
 
 ENDPOINT_MEMBERS = "/congress/v1/{}/{}/members.json"
 ENDPOINT_HOUSE = "/congress/v1/{}/house/members.json"
@@ -29,4 +32,18 @@ def get_house(chamber):
 def get_members(chamber, branch):
     pass
 
-pprint(get_senate(80))
+db = DbManager()
+
+states = ['AL', 'AK', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'DC', 'FL', 'GA', 'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MD', 'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ', 'NM', 'NY', 'NC', 'ND', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VT', 'VA', 'WA', 'WV', 'WI', 'WY']
+
+db.begin()
+for state in states:
+    search_url = 'https://api.propublica.org/campaign-finance/v1/2014/races/{}.json'.format(state)
+    state_candidates = get_data(search_url)
+    for cand in state_candidates['results']:
+        candidate = Candidate()
+        candidate.parse_candidate(cand)
+        cand_data = db.open().query(Candidate).filter(Candidate.fec_id == candidate.fec_id).all()
+        if len(cand_data) == 0:
+            db.save(candidate)
+db.end()
